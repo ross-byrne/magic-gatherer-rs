a6f658f5d6c0f165ba6adebf45fbe8b29ad5c547
Ross Byrne <ross.byrne@outlook.ie>
2023-12-28 14:54:32 +0000

    Working on creating a data dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
+const SCRYFALL_API_URL: &'static str = "https://api.scryfall.com/bulk-data";
+// const DATA_DIR: &'static str = "/data";
+// const CARD_DIR: &'static str = "/data/magic-the-gathering-cards";
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", SCRYFALL_API_URL);
+
+    let current_dir = match env::current_dir() {
+        Ok(path) => path,
+        Err(_e) => panic!("Error reading current path"),
+    };
+
+    println!("The current directory is {}", current_dir.display());
 }
