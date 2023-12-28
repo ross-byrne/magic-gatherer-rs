343112aeac8fc9e1855af465461127155b94b0a1
Ross Byrne <ross.byrne@outlook.ie>
2023-12-28 18:29:39 +0000

    Now parsing response body and finding default cards

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use serde_json::Value;
 use std::fs;
 
 const SCRYFALL_API_URL: &'static str = "https://api.scryfall.com/bulk-data";
@@ -42,5 +43,27 @@ fn fetch_card_data() {
         Err(e) => panic!("{}", e),
     };
 
-    println!("{}", resp_str);
+    let bulk_data_json: Value = match serde_json::from_str(resp_str) {
+        Ok(x) => x,
+        Err(e) => panic!("{}", e),
+    };
+
+    let data = &bulk_data_json["data"];
+    let mut selected_obj: Option<Value> = None;
+
+    for x in data.as_array().unwrap() {
+        // println!("========================================================");
+        // println!("{}", x["type"]);
+
+        if x["type"] == "default_cards" {
+            selected_obj = Some(x.clone());
+            break;
+        }
+    }
+
+    if let Some(x) = selected_obj {
+        println!("{}", x);
+    } else {
+        panic!("Failed to read API data");
+    }
 }
