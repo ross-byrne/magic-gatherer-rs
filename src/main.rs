103f551a45e196b05d23c3621267f8023a357486
Ross Byrne <ross.byrne@outlook.ie>
2024-10-23 16:12:54 +0200

    Now successfully querying the bulk data api from scryfall

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,109 @@
 mod types;
 
-use minreq;
-use serde_json::Value;
+use reqwest;
+use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, USER_AGENT};
+use serde_json::to_string_pretty;
 use std::error::Error;
 use std::fs;
-use std::io;
+use types::BulkData;
 
 const SCRYFALL_API_URL: &'static str = "https://api.scryfall.com/bulk-data";
-const DATA_DIR: &'static str = "data";
+// const DATA_DIR: &'static str = "data";
 const CARD_DIR: &'static str = "data/magic-the-gathering-cards";
 
-fn main() -> Result<(), Box<dyn Error>> {
-    println!("{}", SCRYFALL_API_URL);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    println!("Welcome to magic-gatherer-rs!");
+    let client = reqwest::Client::new();
 
-    create_data_dirs()?;
-    let _download_uri: String = fetch_card_data()?;
+    create_data_dirs();
+    fetch_bulk_data(&client).await?;
 
+    // TODO: clean up
+    // let _download_uri: String = fetch_card_data()?;
     // download_card_json(&download_uri)?;
 
+    println!("\nFinished! :)\n");
     Ok(())
 }
 
-fn create_data_dirs() -> Result<(), io::Error> {
+// Recursively create required data directories
+fn create_data_dirs() {
     println!("Creating data directories...");
 
-    fs::create_dir_all(&DATA_DIR)?;
-    fs::create_dir_all(&CARD_DIR)?;
+    fs::create_dir_all(&CARD_DIR).expect("Creating data directory should be created");
+}
+
+async fn fetch_bulk_data(client: &reqwest::Client) -> Result<(), Box<dyn Error>> {
+    println!("Fetching bulk data from Scryfall API...");
+
+    let bulk_data: BulkData = client
+        .get(SCRYFALL_API_URL)
+        .headers(get_request_headers())
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    // pretty print response for testing
+    let pretty = to_string_pretty(&bulk_data)?;
+    println!("{}", pretty);
 
     Ok(())
 }
 
-fn fetch_card_data() -> Result<String, Box<dyn Error>> {
-    println!("Querying Scryfall bulk api...");
+fn get_request_headers() -> HeaderMap {
+    let mut headers = HeaderMap::new();
 
-    let response = minreq::get(SCRYFALL_API_URL).send()?;
-    let bulk_data_json: Value = response.json()?;
+    // Add headers as requested in API docs: https://scryfall.com/docs/api
+    headers.insert(ACCEPT, HeaderValue::from_static("application/json"));
+    headers.insert(USER_AGENT, HeaderValue::from_static("MagicGatherer/0.1"));
 
-    let default_cards_data: Option<Value> = bulk_data_json["data"]
-        .as_array()
-        .unwrap()
-        .iter()
-        .find(|&x| x["type"] == "default_cards")
-        .cloned();
-
-    if let Some(data) = default_cards_data {
-        let download_uri = data["download_uri"].to_string();
-
-        println!("{}", download_uri);
-
-        Ok(download_uri)
-    } else {
-        panic!("Failed to read API data");
-    }
+    return headers;
 }
 
-fn remove_first_and_last(value: &str) -> &str {
-    let mut chars = value.chars();
-    chars.next();
-    chars.next_back();
+// fn fetch_card_data() -> Result<String, Box<dyn Error>> {
+//     println!("Querying Scryfall bulk api...");
 
-    chars.as_str()
-}
+//     let response = minreq::get(SCRYFALL_API_URL).send()?;
+//     let bulk_data_json: Value = response.json()?;
 
-fn download_card_json(url: &String) -> Result<(), Box<dyn Error>> {
-    println!("Downloading card json...");
+//     let default_cards_data: Option<Value> = bulk_data_json["data"]
+//         .as_array()
+//         .unwrap()
+//         .iter()
+//         .find(|&x| x["type"] == "default_cards")
+//         .cloned();
 
-    // remove "" from json string
-    let _processed_url = remove_first_and_last(&url);
+//     if let Some(data) = default_cards_data {
+//         let download_uri = data["download_uri"].to_string();
 
-    // let response = minreq::get(processed_url).send()?;
-    // let resp_json: Value = response.json()?;
+//         println!("{}", download_uri);
 
-    // println!("{}", resp_json);
+//         Ok(download_uri)
+//     } else {
+//         panic!("Failed to read API data");
+//     }
+// }
 
-    Ok(())
-}
+// fn remove_first_and_last(value: &str) -> &str {
+//     let mut chars = value.chars();
+//     chars.next();
+//     chars.next_back();
+
+//     chars.as_str()
+// }
+
+// fn download_card_json(url: &String) -> Result<(), Box<dyn Error>> {
+//     println!("Downloading card json...");
+
+//     // remove "" from json string
+//     let _processed_url = remove_first_and_last(&url);
+
+//     // let response = minreq::get(processed_url).send()?;
+//     // let resp_json: Value = response.json()?;
+
+//     // println!("{}", resp_json);
+
+//     Ok(())
+// }
--- src/types.rs
+++ src/types.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 pub struct BulkData {
     object: String,
-    has_more: String,
+    has_more: bool,
     data: Vec<BulkDataItem>,
 }
 
